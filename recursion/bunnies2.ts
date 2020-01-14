2db6181fda84e260f5e88dc95dedba8ef82e07fa
Lionel <leo.damian@icloud.com>
2020-01-14 20:31:34 +0100

    adding bunnies2.ts to the recursion workshop folder

---
+++ recursion/bunnies2.ts
@@ -0,0 +1,17 @@
+'use strict'
+
+function numOfEars(ListOfBunnies: number): number {
+
+    if (ListOfBunnies === 1) {
+        return 2;
+    } else {
+        if (ListOfBunnies % 2 === 0) {
+            return 3 + numOfEars(ListOfBunnies - 1);
+        } else {
+            return 2 + numOfEars(ListOfBunnies - 1);
+        }
+    }
+}
+console.log(numOfEars(18));
+
+export { }
