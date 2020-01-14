e81f5b8031600b310efe83daaa555c18a3e040dc
Lionel <leo.damian@icloud.com>
2020-01-14 20:37:10 +0100

    refactoring bunnies2.ts file

--- recursion/bunnies2.ts
+++ recursion/bunnies2.ts
@@ -4,12 +4,10 @@ function numOfEars(ListOfBunnies: number): number {
 
     if (ListOfBunnies === 1) {
         return 2;
+    } else if (ListOfBunnies % 2 === 0) {
+        return 3 + numOfEars(ListOfBunnies - 1);
     } else {
-        if (ListOfBunnies % 2 === 0) {
-            return 3 + numOfEars(ListOfBunnies - 1);
-        } else {
-            return 2 + numOfEars(ListOfBunnies - 1);
-        }
+        return 2 + numOfEars(ListOfBunnies - 1);
     }
 }
 console.log(numOfEars(18));
