5c399432b3bef2688c552a51011074c457c22904
Lionel <leo.damian@icloud.com>
2020-01-14 18:21:15 +0100

    adding recursion workshop files

---
+++ recursion/power.ts
@@ -0,0 +1,13 @@
+'use strict'
+
+function power(base: number, num: number): number {
+    let multiple: number;
+
+    if () {
+        return multiple * base;
+    }
+}
+
+console.log(power(2, 3));
+
+export { }
